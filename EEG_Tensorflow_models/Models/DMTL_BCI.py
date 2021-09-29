e328c82e18d07c1574d021e497b65dff57d9f79d
Julian Caicedo <juccaicedoac@unal.edu.co>
2021-09-28 23:10:14 -0500

    Update DMTL_BCI.py

--- EEG_Tensorflow_models/Models/DMTL_BCI.py
+++ EEG_Tensorflow_models/Models/DMTL_BCI.py
@@ -7,6 +7,7 @@ from tensorflow.keras.constraints import max_norm
 from tensorflow.keras import backend as K
 from tensorflow.keras.layers.experimental.preprocessing import Resizing
 from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.regularizers import l1_l2
 
 
 
@@ -16,27 +17,29 @@ def DMTL_BCI(nb_classes, Chans = 22, Samples = 250, dropoutRate = 0.5):
     strid        = (1,15)
     pool         = (1,75)
     bias_spatial = True
+    l1           = 0
+    l2           = 1e-3
 
     input_main   = Input((Chans, Samples, 1))
     block1       = Conv2D(40, filters, strides=(1,2),
-                                 input_shape=(Chans, Samples, 1),
+                                 input_shape=(Chans, Samples, 1),kernel_regularizer=l1_l2(l1=l1,l2=l2),
                                  kernel_constraint = max_norm(2., axis=(0,1,2)))(input_main)
-    block1       = Conv2D(40, (Chans, 1), use_bias=bias_spatial, 
+    block1       = Conv2D(40, (Chans, 1), use_bias=bias_spatial, kernel_regularizer=l1_l2(l1=l1,l2=l2),
                           kernel_constraint = max_norm(2., axis=(0,1,2)))(block1)
     block1       = BatchNormalization(epsilon=1e-05, momentum=0.1)(block1)
     Act1         = Activation('elu')(block1)
     block1       = AveragePooling2D(pool_size=pool, strides=strid)(Act1)
     block1       = Dropout(dropoutRate,name='bottleneck')(block1)
 
-    ConvC        = Conv2D(nb_classes, (1, block1.shape[2]),kernel_constraint = max_norm(0.5, axis=(0,1,2)),name='ouput')(block1)
+    ConvC        = Conv2D(nb_classes, (1, block1.shape[2]),kernel_regularizer=l1_l2(l1=l1,l2=l2),kernel_constraint = max_norm(0.5, axis=(0,1,2)),name='ouput')(block1)
     flat         = Flatten(name='F_1')(ConvC)
     softmax      = Activation('softmax',name='Classif')(flat)
 
     block2       = Resizing(Act1.shape[1], Act1.shape[2])(block1)
-    block2       = Conv2DTranspose(40, (Chans, 1), use_bias=bias_spatial, 
+    block2       = Conv2DTranspose(40, (Chans, 1), use_bias=bias_spatial, kernel_regularizer=l1_l2(l1=l1,l2=l2),
                           kernel_constraint = max_norm(2., axis=(0,1,2)))(block2)
     block2       = Conv2DTranspose(40, filters,strides=(1,2),
-                                 input_shape=(Chans, Samples, 1),
+                                 input_shape=(Chans, Samples, 1),kernel_regularizer=l1_l2(l1=l1,l2=l2),
                                  kernel_constraint = max_norm(2., axis=(0,1,2)))(block2)
     
     return Model(inputs=input_main, outputs=[block2,softmax])
