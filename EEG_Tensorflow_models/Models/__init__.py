a31b94679df7dcc5e0903109e9aaa03c930cb782
dggarciam <dggarciam@unal.edu.co>
2021-10-01 14:05:49 -0500

    include PST_attention to importables

--- EEG_Tensorflow_models/Models/__init__.py
+++ EEG_Tensorflow_models/Models/__init__.py
@@ -3,3 +3,4 @@ from .EEGNet import EEGNet
 from .ShallowConvNet import ShallowConvNet
 from .DMTL_BCI import DMTL_BCI
 from .TCNet_fusion import TCNet_fusion
+from .PST_attention import PST_attention
