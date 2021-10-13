3c873f12f710f36fc979d1bfe563bea030d60f82
dggarciam <dggarciam@unal.edu.co>
2021-10-12 20:35:21 -0500

    Update __init__.py

--- EEG_Tensorflow_models/Models/__init__.py
+++ EEG_Tensorflow_models/Models/__init__.py
@@ -6,3 +6,4 @@ from .TCNet_fusion import TCNet_fusion
 from .PST_attention import PST_attention
 from .MTVAE import MTVAE
 from .Shallownet_1conv2d import Shallownet_1conv2d
+from .Shallownet_1conv2d_rff import Shallownet_1conv2d_rff
