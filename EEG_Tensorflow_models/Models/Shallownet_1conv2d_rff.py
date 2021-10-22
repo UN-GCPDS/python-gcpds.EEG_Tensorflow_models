e868cc8d7f459718647808df5ab13cd14b153467
dggarciam <dggarciam@unal.edu.co>
2021-10-21 23:04:27 -0500

    Update Shallownet_1conv2d_rff.py

--- EEG_Tensorflow_models/Models/Shallownet_1conv2d_rff.py
+++ EEG_Tensorflow_models/Models/Shallownet_1conv2d_rff.py
@@ -12,16 +12,16 @@ from tensorflow.keras.regularizers import l1_l2
 
 
 
-def Shallownet_1conv2d_rff(nb_classes, Chans = 22, Samples = 250, dropoutRate = 0.5, l1 = 0, l2 = 0):
+def Shallownet_1conv2d_rff(nb_classes, Chans = 22, Samples = 250, dropoutRate = 0.2, l1 = 0, l2 = 0):
 
     filters      = (1,40)
     kernel_time  = 40
-    strid        = (1,15)
+    strid        = (1,5)
     pool         = (1,75)
-    bias_spatial = True
+    bias_spatial = False
 
     input_main   = Input((Chans, Samples, 1))
-    block1       = Conv2D(40, (Chans, kernel_time),strides=(1,2), use_bias=bias_spatial, kernel_regularizer=l1_l2(l1=l1,l2=l2),
+    block1       = Conv2D(20, (Chans, kernel_time),strides=(1,2), use_bias=bias_spatial, kernel_regularizer=l1_l2(l1=l1,l2=l2),
                           kernel_constraint = max_norm(2., axis=(0,1,2)))(input_main)
     block1       = BatchNormalization(epsilon=1e-05, momentum=0.1)(block1)
     block1       = tf.math.cos(block1)
