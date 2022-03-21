9e8bfe11e22975ddc667eed83649c0a038e5f4fc
Julian Caicedo <juccaicedoac@unal.edu.co>
2022-03-20 22:13:57 -0500

    create Min2net

---
+++ EEG_Tensorflow_models/Models/MIN2NET.py
@@ -0,0 +1,6 @@
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Conv2D, AveragePooling2D,Conv2DTranspose
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Input, Flatten,Reshape
+from tensorflow.keras.constraints import max_norm
